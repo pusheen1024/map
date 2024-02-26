045ed476afeeab7009aec9475d406301c237142b
pusheen1024 <114429923+pusheen1024@users.noreply.github.com>
2024-02-26 16:08:19 +0400

    Add files via upload

---
+++ map.py
@@ -0,0 +1,68 @@
+import sys
+from io import BytesIO
+import requests
+from PIL import Image, UnidentifiedImageError
+from PyQt5 import uic
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QPixmap
+
+
+class Map(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('map.ui', self)
+        self.api = API()
+        self.searchButton.clicked.connect(self.search)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp:
+            self.api.change_scale(1)
+        elif event.key() == Qt.Key_PageDown:
+            self.api.change_scale(-1)
+        self.search()
+
+    def wheelEvent(self, event):
+        if event.angleDelta().y() > 0:
+            self.api.change_scale(1)
+        elif event.angleDelta().y() < 0:
+            self.api.change_scale(-1)
+        self.search()
+
+    def search(self):
+        lon = self.longitude.value()
+        lat = self.latitude.value()
+        try:
+            result = self.api.search_by_coords(lon, lat)
+            Image.open(BytesIO(result)).save('temp.png')
+            self.mapLabel.setPixmap(QPixmap('temp.png'))
+        except UnidentifiedImageError:
+            self.statusBar().showMessage('Возникла ошибка при работе с API!')
+
+
+class API:
+    def __init__(self):
+        self.api_server = "http://static-maps.yandex.ru/1.x/"
+        self.z = 12
+        self.type = 'map'
+
+    def search_by_coords(self, lon, lat):
+        self.params = {'l': self.type, 'll': ','.join([str(lon), str(lat)]), 'z': self.z}
+        response = requests.get(self.api_server, params=self.params)
+        return response.content
+
+    def change_scale(self, incr):
+        if incr > 0 and self.z < 15 or incr < 0 and self.z > 0:
+            self.z += incr
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = Map()
+    window.show()
+    sys.excepthook = except_hook
+    app.exec()
