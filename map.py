98b92396a77ab76658e1a1ad73fe49fdc10ef839
pusheen1024 <114429923+pusheen1024@users.noreply.github.com>
2024-03-10 22:21:58 +0400

    Add files via upload

--- map.py
+++ map.py
@@ -14,13 +14,25 @@ class Map(QMainWindow):
         uic.loadUi('map.ui', self)
         self.api = API()
         self.searchButton.clicked.connect(self.search)
+        for button in self.buttonGroup.buttons():
+            button.clicked.connect(self.change_type)
+        self.mapButton.setChecked(True)
+        self.search()
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_PageUp:
             self.api.change_scale(1)
         elif event.key() == Qt.Key_PageDown:
             self.api.change_scale(-1)
-        self.search()
+        elif event.key() == Qt.Key_W:
+            self.api.move_map(0, 1)
+        elif event.key() == Qt.Key_S:
+            self.api.move_map(0, -1)
+        elif event.key() == Qt.Key_A:
+            self.api.move_map(1, 0)
+        elif event.key() == Qt.Key_D:
+            self.api.move_map(-1, 0)
+        self.search(new_search=False)
 
     def wheelEvent(self, event):
         if event.angleDelta().y() > 0:
@@ -29,32 +41,51 @@ class Map(QMainWindow):
             self.api.change_scale(-1)
         self.search()
 
-    def search(self):
+    def search(self, new_search=True):
         lon = self.longitude.value()
         lat = self.latitude.value()
         try:
-            result = self.api.search_by_coords(lon, lat)
+            result = self.api.search_by_coords(lon, lat, new_search)
             Image.open(BytesIO(result)).save('temp.png')
             self.mapLabel.setPixmap(QPixmap('temp.png'))
         except UnidentifiedImageError:
             self.statusBar().showMessage('Возникла ошибка при работе с API!')
 
+    def change_type(self):
+        if self.sender().text() == 'Карта':
+            map_type = 'map'
+        if self.sender().text() == 'Спутник':
+            map_type = 'sat'
+        if self.sender().text() == 'Гибрид':
+            map_type = 'sat,skl'
+        self.api.change_type(map_type)
+        self.search()
+
 
 class API:
     def __init__(self):
         self.api_server = "http://static-maps.yandex.ru/1.x/"
+        self.lon_size, self.lat_size = 180, 90
         self.z = 12
         self.type = 'map'
 
-    def search_by_coords(self, lon, lat):
-        self.params = {'l': self.type, 'll': ','.join([str(lon), str(lat)]), 'z': self.z}
+    def search_by_coords(self, lon, lat, new_search=True):
+        if new_search:
+            self.lon, self.lat = lon, lat
+        self.params = {'l': self.type, 'll': ','.join([str(self.lon), str(self.lat)]), 'z': self.z}
         response = requests.get(self.api_server, params=self.params)
-        return response.content
+        return response.content    
+
+    def change_type(self, map_type):
+        self.type = map_type
 
     def change_scale(self, incr):
-        if incr > 0 and self.z < 15 or incr < 0 and self.z > 0:
+        if incr > 0 and self.z < 15 or incr < 0 and self.z > 1:
             self.z += incr
 
+    def move_map(self, dx, dy):
+        self.lon += dx * self.lon_size / 2 ** (self.z - 1)
+        self.lat += dy * self.lat_size / 2 ** (self.z - 1)
 
 def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
